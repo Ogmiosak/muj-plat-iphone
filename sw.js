b9ec72e7977d2439ba5682f3bc9b842f018a8dd4
Ogmiosak <162766327+Ogmiosak@users.noreply.github.com>
2026-09-14 14:12:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "muj-plat-iphone-pwa-0.0.30";
+const CACHE = "muj-plat-iphone-pwa-0.0.31";
 const FILES = ["./", "./index.html", "./manifest.json", "./app_icon.png"];
 
 self.addEventListener("install", event => {
